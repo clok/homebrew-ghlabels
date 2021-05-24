b0bba544cf1a1208cd79e2b2da3b0541ad439663
goreleaserbot <goreleaser@carlosbecker.com>
2021-05-24 11:31:38 -0500

    Brew formula update for ghlabels version v1.1.0

--- ghlabels.rb
+++ ghlabels.rb
@@ -5,29 +5,29 @@
 class Ghlabels < Formula
   desc "Simple CLI tool to help manage labels across repos"
   homepage "https://clokwork.net/ghlabels/"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/clok/ghlabels/releases/download/v1.0.0/ghlabels_1.0.0_darwin_amd64.tar.gz"
-    sha256 "2715746b0bdbcb0c17219bb3af25bf54ac6cf45cbedfd21b7fc50c414c692793"
+    url "https://github.com/clok/ghlabels/releases/download/v1.1.0/ghlabels_1.1.0_darwin_amd64.tar.gz"
+    sha256 "f1e0409591af8c86d723ce09d21738d33eb6f71a57c566c29a234ba0a3b511a1"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/clok/ghlabels/releases/download/v1.0.0/ghlabels_1.0.0_darwin_arm64.tar.gz"
-    sha256 "ed9df407fb06ebf0dcf84ee10c80edcf98f297a51deb876bda4596e054cf4e5d"
+    url "https://github.com/clok/ghlabels/releases/download/v1.1.0/ghlabels_1.1.0_darwin_arm64.tar.gz"
+    sha256 "0c497f484dd4f1f6584855d37ed54eeb4923a70cc4ad1a3f5c7db4119e80c7f7"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/clok/ghlabels/releases/download/v1.0.0/ghlabels_1.0.0_linux_amd64.tar.gz"
-    sha256 "9cebce7639f6ef6e8d4fe5a196a4c91068324e87be5e3f9020a08f02116fe9b4"
+    url "https://github.com/clok/ghlabels/releases/download/v1.1.0/ghlabels_1.1.0_linux_amd64.tar.gz"
+    sha256 "155b8ee4490343a9e20d5d9026305bd639201f57b113b790ac417349f032c4a4"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/clok/ghlabels/releases/download/v1.0.0/ghlabels_1.0.0_linux_armv6.tar.gz"
-    sha256 "ba08ca8a711f1b4370fc3b8845d14a4a04d341dd07758feadc1d76def2bf542f"
+    url "https://github.com/clok/ghlabels/releases/download/v1.1.0/ghlabels_1.1.0_linux_armv6.tar.gz"
+    sha256 "2c7811e1926c8cbed02c2f193ec4cce078350994438d50fbe4f2926cc2ad2de7"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/clok/ghlabels/releases/download/v1.0.0/ghlabels_1.0.0_linux_arm64.tar.gz"
-    sha256 "e2c825ce9f95dbc49c65b37839971f30a634335b5edc537f5d371eb340d7f4a8"
+    url "https://github.com/clok/ghlabels/releases/download/v1.1.0/ghlabels_1.1.0_linux_arm64.tar.gz"
+    sha256 "405eead95db8b3d29f0e5dc1671ce8cefbfbb337b455a7ee6e462b7374fb21d8"
   end
 
   def install
